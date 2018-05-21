791dc3bf79ece818af8b5033dd4c091dc2171e53
Carlos Pambo <carlos_pambo@Carlos-Pambos-MacBook-Pro.local>
2018-05-21 10:22:21 +0200

    .

--- resources/views/pages/clock.blade.php
+++ resources/views/pages/clock.blade.php
@@ -41,7 +41,7 @@
                 <div class="row">
                     <div class="form-group col-md-3">
                         <a href="/" class="btn btn-white btn-cons btn-block " type="submit">Back</a>
-                    </div>s
+                    </div>
                     <div class="form-group col-md-5"></div>
                     <div class="form-group col-md-4">
                         <button class="btn btn-warning btn-cons btn-block" type="submit"> <i class="fa fa-check"></i> &nbsp; Register Clock</button>
